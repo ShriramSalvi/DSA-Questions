81a5a7ac65dbb776646fb40abdfba6e785950710
ShriramSalvi <bhikajisalavi12345@gmail.com>
2026-08-09 15:37:50 +0530

    Time: 1 ms (16.49%), Space: 43.1 MB (16.72%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+
+       HashMap<Character,Integer>mp= new HashMap<>();
+
+       for(char ch:s.toCharArray()){
+         mp.put(ch,mp.getOrDefault(ch,0)+1);
+
+         if(mp.get(ch)==2)return ch;
+       }
+       return 'a';
+    }
+}
